74bd687e3dda279fce9d4e1fdb0aa9e315616ff2
Tarylin <jiskelbasumatary41@gmail.com>
2021-02-19 21:18:14 +0530

    name

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-var hypnoticBall, database;
-var position;
+var database;
+var gameState = 0;
+var playerCount;
+var form,player,game;
 
 
 function setup(){
@@ -7,30 +9,17 @@ function setup(){
   console.log(database);
   createCanvas(500,500);
 
-  hypnoticBall = createSprite(250,250,10,10);
-  hypnoticBall.shapeColor = "red";
+  game =new Game();
+ game.getState();
+ game.start();
 
-
-  var hypnoticBallPosition = database.ref('ball/position');
-  hypnoticBallPosition.on("value", readPosition, showError);
+  
 }
 
 function draw(){
   background("white");
   
-    if(keyDown(LEFT_ARROW)){
-      writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-      writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-      writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-      writePosition(0,+1);
-    }
-    drawSprites();
+   
   
 }
 
